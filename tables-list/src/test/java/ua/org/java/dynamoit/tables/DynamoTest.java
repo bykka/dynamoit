88bf4d98387d7234fdd94df24737bd341979cfd8
Pavlo Iatsiuk <pavel.yatsuk@gmail.com>
2020-01-30 08:16:14 +0100

    copy data

--- tables-list/src/test/java/ua/org/java/dynamoit/tables/DynamoTest.java
+++ tables-list/src/test/java/ua/org/java/dynamoit/tables/DynamoTest.java
@@ -1,17 +1,39 @@
 package ua.org.java.dynamoit.tables;
 
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
 
 public class DynamoTest {
 
     @Test
-    public void testTables(){
+    public void testTables() {
 //        DynamoDbClient dynamoDbClient = DynamoDbClient.create();
 //        ListTablesResponse listTablesResponse = dynamoDbClient.listTables();
 //
 //        System.out.println(listTablesResponse.tableNames());
+    }
+
+    @Test
+    public void testCopyVenue() {
+//        DynamoDbClient dynamoDbClient = DynamoDbClient.create();
+//        Set<String> venueIds = dynamoDbClient.scan(ScanRequest.builder().tableName("oms.prod.venues").build())
+//                .items()
+//                .stream()
+//                .map(item -> item.get("id"))
+//                .filter(Objects::nonNull)
+//                .map(AttributeValue::s)
+//                .collect(Collectors.toSet());
+//        ScanResponse excursions = dynamoDbClient.scan(ScanRequest.builder().tableName("excursions").build());
+//
+//        Set<Map<String, AttributeValue>> broken = excursions.items().stream()
+//                .filter(item -> Optional.ofNullable(item.get("venueId"))
+//                        .map(AttributeValue::s)
+//                        .map(id -> !venueIds.contains(id))
+//                        .orElse(true))
+//                .collect(Collectors.toSet());
+//
+//        broken.forEach(item->{
+//            System.out.println(item.get("id"));
+//        });
     }
 
 }
