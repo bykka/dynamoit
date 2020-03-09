21a17a2cf88d3563e8a05be7f18f122c7d6b2ba5
Pavlo Iatsiuk <pavlo.iatsiuk@n-ems.com>
2020-03-09 10:53:09 +0100

    use query if hash key is defined

--- src/main/java/ua/org/java/dynamoit/table/TableController.java
+++ src/main/java/ua/org/java/dynamoit/table/TableController.java
@@ -5,8 +5,10 @@ import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.dynamodbv2.model.DescribeTableResult;
+import com.amazonaws.util.StringUtils;
 import javafx.beans.property.SimpleStringProperty;
 import ua.org.java.dynamoit.db.DynamoDBService;
+import ua.org.java.dynamoit.utils.Utils;
 
 import java.util.List;
 import java.util.Map;
@@ -20,6 +22,8 @@ public class TableController {
     private DynamoDB documentClient;
     private TableContext context;
     private CompletableFuture<DescribeTableResult> describeTableResult;
+    private String hashAttribute;
+    private String rangeAttribute;
 
     public TableController(TableContext context, DynamoDBService dynamoDBService) {
         this.context = context;
@@ -27,9 +31,13 @@ public class TableController {
         documentClient = dynamoDBService.getOrCreateDocumentClient(context.getProfileName());
 
         describeTableResult = CompletableFuture.supplyAsync(() -> dbClient.describeTable(context.getTableName()));
+        describeTableResult.thenAccept(describeTable -> {
+            Utils.getHashKey(describeTable).ifPresent(s -> hashAttribute = s);
+            Utils.getHashKey(describeTable).ifPresent(s -> rangeAttribute = s);
+        });
     }
 
-    private CompletableFuture<ItemCollection<ScanOutcome>> scanItems(Map<String, SimpleStringProperty> attributeFilterMap) {
+    private CompletableFuture<Page<Item, ?>> scanItems(Map<String, SimpleStringProperty> attributeFilterMap) {
         Table table = documentClient.getTable(context.getTableName());
         return CompletableFuture.supplyAsync(() -> {
             ScanSpec scanSpec = new ScanSpec();
@@ -40,39 +48,35 @@ public class TableController {
             if (!filters.isEmpty()) {
                 scanSpec.withScanFilters(filters.toArray(new ScanFilter[]{}));
             }
-            return table.scan(scanSpec.withMaxPageSize(100));
+            return table.scan(scanSpec.withMaxPageSize(100)).firstPage();
         });
     }
 
     public CompletableFuture<Page<Item, ?>> queryPageItems(Map<String, SimpleStringProperty> attributeFilterMap) {
-        Table table = documentClient.getTable(context.getTableName());
-
-        return CompletableFuture.supplyAsync(() -> {
-            ScanSpec scanSpec = new ScanSpec();
-            List<ScanFilter> filters = attributeFilterMap.entrySet().stream()
-                    .filter(entry -> Objects.nonNull(entry.getValue().get()) && entry.getValue().get().trim().length() > 0)
-                    .map(entry -> new ScanFilter(entry.getKey()).eq(entry.getValue().get()))
-                    .collect(Collectors.toList());
-            if (!filters.isEmpty()) {
-                scanSpec.withScanFilters(filters.toArray(new ScanFilter[]{}));
-            }
-            ItemCollection<ScanOutcome> scan = table.scan(scanSpec.withMaxPageSize(100));
-            return scan.firstPage();
-        });
+        SimpleStringProperty hashValueProperty = attributeFilterMap.get(hashAttribute);
+        if (hashValueProperty != null && !StringUtils.isNullOrEmpty(hashValueProperty.get())) {
+            return queryItems(attributeFilterMap);
+        }
+        return scanItems(attributeFilterMap);
     }
 
-    private CompletableFuture<ItemCollection<QueryOutcome>> queryItems(Map<String, SimpleStringProperty> attributeFilterMap) {
+    private CompletableFuture<Page<Item, ?>> queryItems(Map<String, SimpleStringProperty> attributeFilterMap) {
         Table table = documentClient.getTable(context.getTableName());
         return CompletableFuture.supplyAsync(() -> {
             QuerySpec querySpec = new QuerySpec();
+            querySpec.withHashKey(hashAttribute, attributeFilterMap.get(hashAttribute).get());
+            if (!StringUtils.isNullOrEmpty(attributeFilterMap.get(rangeAttribute).get())) {
+                querySpec.withRangeKeyCondition(new RangeKeyCondition(rangeAttribute).eq(attributeFilterMap.get(rangeAttribute).get()));
+            }
             List<QueryFilter> filters = attributeFilterMap.entrySet().stream()
-                    .filter(entry -> Objects.nonNull(entry.getValue().get()) && entry.getValue().get().trim().length() > 0)
+                    .filter(entry -> !entry.getKey().equals(hashAttribute) && !entry.getKey().equals(rangeAttribute))
+                    .filter(entry -> !StringUtils.isNullOrEmpty(entry.getValue().get()))
                     .map(entry -> new QueryFilter(entry.getKey()).eq(entry.getValue().get()))
                     .collect(Collectors.toList());
             if (!filters.isEmpty()) {
                 querySpec.withQueryFilters(filters.toArray(new QueryFilter[]{}));
             }
-            return table.query(querySpec.withMaxPageSize(100));
+            return table.query(querySpec.withMaxPageSize(100)).firstPage();
         });
     }
 
--- src/main/java/ua/org/java/dynamoit/utils/Utils.java
+++ src/main/java/ua/org/java/dynamoit/utils/Utils.java
@@ -1,8 +1,10 @@
 package ua.org.java.dynamoit.utils;
 
 import com.amazonaws.services.dynamodbv2.model.DescribeTableResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.TableDescription;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -20,6 +22,16 @@ public class Utils {
     public static boolean isRangeKey(String attributeName, DescribeTableResult describeTableResult) {
         TableDescription tableDescription = describeTableResult.getTable();
         return tableDescription.getKeySchema().stream().anyMatch(keySchemaElement -> keySchemaElement.getAttributeName().equals(attributeName) && keySchemaElement.getKeyType().equals("RANGE"));
+    }
+
+    public static Optional<String> getHashKey(DescribeTableResult describeTableResult) {
+        TableDescription tableDescription = describeTableResult.getTable();
+        return tableDescription.getKeySchema().stream().filter(keySchemaElement -> keySchemaElement.getKeyType().equals("HASH")).map(KeySchemaElement::getAttributeName).findFirst();
+    }
+
+    public static Optional<String> getRangeKey(DescribeTableResult describeTableResult) {
+        TableDescription tableDescription = describeTableResult.getTable();
+        return tableDescription.getKeySchema().stream().filter(keySchemaElement -> keySchemaElement.getKeyType().equals("RANGE")).map(KeySchemaElement::getAttributeName).findFirst();
     }
 
 }
