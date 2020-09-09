30627b8f4b5e8d199625487897bad3a2240efede
Pavlo Iatsiuk <pavel.yatsuk@gmail.com>
2020-09-09 23:47:57 +0200

    dynamoit-4 Search in the edit document dialog

--- src/main/java/ua/org/java/dynamoit/components/tablegrid/ItemDialog.java
+++ src/main/java/ua/org/java/dynamoit/components/tablegrid/ItemDialog.java
@@ -22,7 +22,6 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.rxjavafx.observables.JavaFxObservable;
 import io.reactivex.rxjavafx.observers.JavaFxObserver;
 import io.reactivex.rxjavafx.schedulers.JavaFxScheduler;
-import io.reactivex.schedulers.Schedulers;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.*;
@@ -32,16 +31,21 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import org.controlsfx.control.textfield.TextFields;
 import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.Selection;
+import org.fxmisc.richtext.SelectionImpl;
 import org.reactfx.EventStream;
 import org.reactfx.Subscription;
 import ua.org.java.dynamoit.components.jsoneditor.JsonEditor;
 import ua.org.java.dynamoit.utils.DX;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -55,33 +59,47 @@ public class ItemDialog extends Dialog<String> {
     private final ToolBar toolBar;
     private Subscription validationSubscribe;
     private final JsonEditor textArea = new JsonEditor();
-    private TextField searchField;
+    private final TextField searchField = TextFields.createClearableTextField();
     private Disposable focusDisposable;
+    private final List<Selection<Collection<String>, String, Collection<String>>> selections = new ArrayList<>();
 
     public ItemDialog(String title, String json, Function<EventStream<String>, EventStream<Boolean>> validator) {
         this.setTitle(title);
 
+        Observable<List<Pair<Integer, String>>> matches = JavaFxObservable.changesOf(this.searchField.textProperty())
+                .debounce(100, TimeUnit.MILLISECONDS)
+                .map(stringChange -> {
+                            AtomicInteger index = new AtomicInteger(0);
+                            return textArea.getText().lines()
+                                    .map(line -> new Pair<>(index.getAndIncrement(), line))
+                                    .filter(pair -> !pair.getValue().isBlank() && !stringChange.getNewVal().isBlank() && pair.getValue().contains(stringChange.getNewVal()))
+                                    .collect(Collectors.toList());
+                        }
+                )
+                .observeOn(JavaFxScheduler.platform());
+
+        matches.subscribe(list -> {
+            cleanAllSelections();
+            list.forEach(line -> {
+                Integer key = line.getKey();
+                textArea.moveTo(key, 0);
+                Selection<Collection<String>, String, Collection<String>> selection = new SelectionImpl<>("match" + key, textArea, path -> {
+                    path.setStroke(Color.GREEN);
+                    path.setStrokeWidth(1);
+                    path.setHighlightFill(Color.LIGHTYELLOW);
+                });
+                textArea.addSelection(selection);
+                selection.selectRange(key, 0, key, line.getValue().length());
+                selections.add(selection);
+            });
+        });
+
         this.toolBar = DX.toolBar(toolBar -> List.of(
-                DX.create(TextFields::createClearableTextField, textField -> {
-                    this.searchField = textField;
+                DX.create(() -> this.searchField, textField -> {
                     HBox.setHgrow(textField, Priority.ALWAYS);
-                    textField.textProperty().addListener((observable, oldValue, newValue) -> {
-                        AtomicInteger index = new AtomicInteger();
-                        List<Pair<Integer, String>> matches = textArea.getText().lines()
-                                .map(line -> new Pair<>(index.getAndIncrement(), line))
-                                .filter(pair -> pair.getValue().contains(newValue))
-                                .collect(Collectors.toList());
-                    });
                 }),
                 DX.create(Label::new, label -> {
-                    Observable<String> count = JavaFxObservable.changesOf(this.searchField.textProperty())
-                            .map(stringChange -> textArea.getText().lines()
-                                    .filter(line -> line.contains(stringChange.getNewVal()))
-                                    .collect(Collectors.toList()))
-                            .map(list -> list.size() + " matches");
-
-                    label.textProperty().bind(JavaFxObserver.toBinding(count));
-
+                    label.textProperty().bind(JavaFxObserver.toBinding(matches.map(list -> list.size() + " matches")));
                 }),
                 DX.create(Button::new, button -> {
                     button.setTooltip(new Tooltip("Hide toolbar"));
@@ -147,13 +165,19 @@ public class ItemDialog extends Dialog<String> {
         });
     }
 
+    private void cleanAllSelections() {
+        selections.forEach(selection -> {
+            selection.deselect();
+            textArea.removeSelection(selection);
+        });
+        selections.clear();
+    }
 
     private void showToolbar() {
         if (!this.mainBox.getChildren().contains(this.toolBar)) {
             this.mainBox.getChildren().add(0, this.toolBar);
 
-            focusDisposable = Observable.timer(10, TimeUnit.MILLISECONDS)
-                    .subscribeOn(Schedulers.io())
+            focusDisposable = Observable.timer(100, TimeUnit.MILLISECONDS)
                     .observeOn(JavaFxScheduler.platform())
                     .subscribe(aLong -> this.searchField.requestFocus());
         }
@@ -163,5 +187,7 @@ public class ItemDialog extends Dialog<String> {
         this.mainBox.getChildren().remove(this.toolBar);
         this.textArea.requestFocus();
         this.focusDisposable.dispose();
+        this.searchField.clear();
+        this.cleanAllSelections();
     }
 }
