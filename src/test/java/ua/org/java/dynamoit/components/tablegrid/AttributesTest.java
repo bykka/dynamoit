8adc650b00c2de9e2140756c97e82df7377a42c0
Pavlo Iatsiuk <pavel.yatsuk@gmail.com>
2020-10-17 23:12:23 +0200

    fixed filter builder for string attributes

---
+++ src/test/java/ua/org/java/dynamoit/components/tablegrid/AttributesTest.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of DynamoIt.
+ *
+ *     DynamoIt is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DynamoIt is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with DynamoIt.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.org.java.dynamoit.components.tablegrid;
+
+import com.amazonaws.services.dynamodbv2.document.ScanFilter;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AttributesTest {
+
+    @Test
+    public void testAttributeValueToFilter() {
+        ScanFilter scanFilter = Attributes.attributeValueToFilter("name", "John", Attributes.Type.STRING, ScanFilter::new);
+        assertEquals(ComparisonOperator.EQ, scanFilter.getComparisonOperator());
+        assertArrayEquals(new Object[]{"John"}, scanFilter.getValues());
+
+        scanFilter = Attributes.attributeValueToFilter("name", "John*", Attributes.Type.STRING, ScanFilter::new);
+        assertEquals(ComparisonOperator.BEGINS_WITH, scanFilter.getComparisonOperator());
+        assertArrayEquals(new Object[]{"John"}, scanFilter.getValues());
+
+        scanFilter = Attributes.attributeValueToFilter("name", "*John*", Attributes.Type.STRING, ScanFilter::new);
+        assertEquals(ComparisonOperator.CONTAINS, scanFilter.getComparisonOperator());
+        assertArrayEquals(new Object[]{"John"}, scanFilter.getValues());
+
+        scanFilter = Attributes.attributeValueToFilter("name", null, Attributes.Type.STRING, ScanFilter::new);
+        assertNull(scanFilter.getComparisonOperator());
+
+        scanFilter = Attributes.attributeValueToFilter("name", "", Attributes.Type.STRING, ScanFilter::new);
+        assertNull(scanFilter.getComparisonOperator());
+
+        scanFilter = Attributes.attributeValueToFilter("name", "*", Attributes.Type.STRING, ScanFilter::new);
+        assertNull(scanFilter.getComparisonOperator());
+
+        scanFilter = Attributes.attributeValueToFilter("name", "**", Attributes.Type.STRING, ScanFilter::new);
+        assertNull(scanFilter.getComparisonOperator());
+    }
+}
+
