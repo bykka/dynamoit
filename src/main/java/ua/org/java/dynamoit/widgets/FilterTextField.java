1e2d19b07a9f86622b05f0801e39cd7004701d32
Pavlo Iatsiuk <pavel.yatsuk@gmail.com>
2020-11-09 00:54:41 +0100

    introduced FilterTextField

---
+++ src/main/java/ua/org/java/dynamoit/widgets/FilterTextField.java
@@ -0,0 +1,84 @@
+/*
+ * This file is part of DynamoIt.
+ *
+ *     DynamoIt is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DynamoIt is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with DynamoIt.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.org.java.dynamoit.widgets;
+
+import javafx.geometry.Bounds;
+import javafx.scene.Cursor;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import ua.org.java.dynamoit.utils.DX;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FilterTextField extends ClearableTextField {
+
+    public FilterTextField() {
+        Label label = new Label("==");
+        StackPane pane = new StackPane(label);
+        pane.setCursor(Cursor.DEFAULT);
+        pane.setOnMouseClicked(event -> {
+            Bounds localBounds = this.getBoundsInLocal();
+            Bounds screenBounds = this.localToScreen(localBounds);
+            DX.contextMenu(contextMenu ->
+                    Stream.of(Operation.values())
+                            .map(operation -> DX.create(CheckMenuItem::new, menu -> {
+                                menu.setText(operation.getText());
+                                menu.setGraphic(new Label(operation.getLabel()));
+                                menu.setOnAction(__ -> label.setText(operation.getLabel()));
+//                                menu.selectedProperty().bind(Bindings.equal(label.textProperty(), operation.getLabel()));
+                            }))
+                            .collect(Collectors.toList())
+            ).show(pane, screenBounds.getMinX(), screenBounds.getMinY() + getHeight());
+        });
+
+        setLeft(pane);
+    }
+
+    private enum Operation {
+        EQUALS("==", "equals"),
+        NOT_EQUALS("!=", "not equals"),
+        LESS_THAN("<", "less than"),
+        LESS_THAN_OR_EQUALS("<=", "less than or equals"),
+        MORE_THAN(">", "more than"),
+        MORE_THAN_OR_EQUALS("=>", "more than or equals"),
+        BETWEEN("...", "between"),
+        EXISTS("$", "exists"),
+        NOT_EXISTS("!$", "not exists"),
+        CONTAINS("~", "contains"),
+        NOT_CONTAINS("!~", "not contains"),
+        BEGINS_WITH("^", "begins with");
+
+        private final String label;
+        private final String text;
+
+        Operation(String label, String text) {
+            this.label = label;
+            this.text = text;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public String getText() {
+            return text;
+        }
+    }
+}
