c5a0380ef0777a7a4b403d3be22f256a488b6eef
Pavlo Iatsiuk <pavlo.iatsiuk@nems.eco>
2026-01-15 22:52:05 +0100

    fixed testcases

--- src/test/java/ua/org/java/dynamoit/components/tablegrid/TableGridControllerTest.java
+++ src/test/java/ua/org/java/dynamoit/components/tablegrid/TableGridControllerTest.java
@@ -17,7 +17,6 @@
 
 package ua.org.java.dynamoit.components.tablegrid;
 
-import javafx.application.HostServices;
 import org.junit.jupiter.api.Test;
 import ua.org.java.dynamoit.DynamoDBTest;
 import ua.org.java.dynamoit.EventBus;
@@ -32,11 +31,7 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 
-
-//@PrepareForTest(HostServices.class)
-//@RunWith(PowerMockRunner.class)
 public class TableGridControllerTest extends DynamoDBTest {
 
     @Test
@@ -59,7 +54,6 @@ public class TableGridControllerTest extends DynamoDBTest {
 
         TableGridContext context = new TableGridContext(profileDetails, "Users");
 
-        HostServices hostServices = mock(HostServices.class);
         EventBus eventBus = new EventBus(ForkJoinPool.commonPool());
 
         TableGridController controller = new TableGridModule() {
@@ -67,19 +61,10 @@ public class TableGridControllerTest extends DynamoDBTest {
             protected Executor getUIExecutor() {
                 return ForkJoinPool.commonPool();
             }
-        }.controller(context, model, dynamoDbClientRegistry, eventBus, hostServices);
+        }.controller(context, model, dynamoDbClientRegistry, eventBus, null);
 
-//        expect(controller.queryPageItems()).andReturn(CompletableFuture.completedFuture(
-//                new Pair<>(List.of(new Item(), new Item()), page)
-//        ));
-//
-//        replay(controller);
-//
         controller.onRefreshData().join();
-//
-//        verify(table, amazonDynamoDB, dynamoDB, dynamoDBService, page, controller);
-//
+
         assertEquals(2, model.getRowsSize());
-//        assertEquals(model.getPageIterator(), page);
     }
 }
